1447448cfe628b5492a58b2917efe8b23724a0ed
Jasmine dhunna <jasmine9003@gmail.com>
2019-01-22 13:50:42 -0800

    new changes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+
 import './App.css';
+import {DISHES} from './shared/dishes'
 
 class App extends Component {
+constructor(props){
+  super(props);
+  this.state={
+    dishes:DISHES};
+  }
+
+
+
   render() {
     return (
       <div>
@@ -16,7 +26,7 @@ class App extends Component {
         <NavbarBrand href="/">ristorante2</NavbarBrand>
         </div>
         </Navbar>
-        <Menu/>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
